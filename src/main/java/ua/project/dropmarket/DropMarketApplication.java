705419d7b184e15002a322518d131d4e096fc024
Vadim Voitsekhovskiy <vadim.rolex.2005@outlook.com>
2024-04-09 17:55:18 +0300

    First point of this project.

---
+++ src/main/java/ua/project/dropmarket/DropMarketApplication.java
@@ -0,0 +1,12 @@
+package ua.project.dropmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropMarketApplication.class, args);
+    }
+}
