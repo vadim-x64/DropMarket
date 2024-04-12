3f445f25465f5f54e2b4bc7152805a096b2b748a
Vadim Voitsekhovskiy <vadim.rolex.2005@outlook.com>
2024-04-12 22:41:27 +0300

    The field of search was added to this project.

--- src/main/java/ua/project/dropmarket/entity/Product.java
+++ src/main/java/ua/project/dropmarket/entity/Product.java
@@ -19,6 +19,7 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    private boolean available;
 
     private LocalDateTime createdAt;
 
