5d6872f808a06088530d0c4173f56df5183c0990
Vadim Voitsekhovskiy <vadim.rolex.2005@outlook.com>
2024-04-14 13:27:43 +0300

    Main page.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,11 @@
-document.getElementById('scrollIcon').addEventListener('click', function () {
-    const container = document.querySelector('.cat');
-    container.scrollIntoView({behavior: 'smooth'});
+document.getElementById('scrollIcon').addEventListener('click', function (event) {
+    event.preventDefault();
+    const catSection = document.querySelector('.cat');
+    catSection.scrollIntoView({ behavior: 'smooth' });
 });
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const showMoreButton = document.getElementById('showMoreButton');
-    const cardsContainer = document.querySelector('.cat');
     const cards = document.querySelectorAll('.card');
     const cardsToShow = 9;
     let visibleCards = cardsToShow;
@@ -15,16 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let i = visibleCards; i < visibleCards + cardsToShow && i < cards.length; i++) {
             cards[i].classList.remove('d-none');
         }
-
         visibleCards += cardsToShow;
 
-        // Перевіряємо, чи всі карточки вже відображені
         if (visibleCards >= cards.length) {
             showMoreButton.style.display = 'none';
         }
     });
 
-    // Функція для відображення карточок товарів одну за одною
     function showCardsSequentially() {
         cards.forEach(function (card, index) {
             if (index < visibleCards) {
@@ -34,11 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-
-    // Приховує всі карточки, крім перших cardsToShow
     showCardsSequentially();
 
-    // Перевіряє, чи потрібно відображати кнопку "Показати ще"
     if (cards.length <= cardsToShow) {
         showMoreButton.style.display = 'none';
     }
@@ -49,7 +42,6 @@ document.getElementById('searchInput').addEventListener('input', function () {
     const cards = document.querySelectorAll('.card');
     const showMoreButton = document.getElementById('showMoreButton');
     const notFoundMessage = document.getElementById('notFoundMessage');
-
     let foundItems = false;
 
     cards.forEach(function (card) {
@@ -70,22 +62,15 @@ document.getElementById('searchInput').addEventListener('input', function () {
     }
 });
 
-
 document.getElementById('searchInput').addEventListener('keypress', function (event) {
-    // Перевірка, чи натиснута клавіша Enter
     if (event.key === 'Enter') {
-        // Отримання блока, до якого потрібно прокрутити
         const featuretteBlock = document.querySelector('.cat');
-        // Плавний перехід до блока
         featuretteBlock.scrollIntoView({behavior: 'smooth'});
     }
 });
 
-
-// Оголошуємо глобальну змінну для username
 let username = "";
 
-// Функція для генерації випадкового кольору
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -96,22 +81,16 @@ function getRandomColor() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Отримуємо значення username з метаданих сторінки
     username = document.querySelector('meta[name="username"]').getAttribute('content');
 
     const userInitial = document.getElementById('userInitial');
-
-    // Перевірка, чи вже був збережений колір для цього користувача
     let userColor = localStorage.getItem(username + '_color');
 
-    // Генерація нового кольору, якщо він ще не збережений
     if (!userColor) {
         userColor = getRandomColor();
-        // Збереження кольору для поточного користувача
         localStorage.setItem(username + '_color', userColor);
     }
 
-    // Встановлення кольору та першої букви для віконця акаунту
     if (username) {
         const firstLetter = username.charAt(0).toUpperCase();
         userInitial.innerText = firstLetter;
